387d7133db8d5d7a9506521221b5c39aab43a128
kimmingyu <minku4820@gmail.com>
2022-10-14 01:37:55 +0900

    css fix, (404,index)html 수정

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
   return (
     <div className={color ? "header header-bg":"header"}>
       <Link to="/">
-        <h1>FE Dveloper Portfolo</h1>
+        <h1>KMG's Portfolio</h1>
       </Link>
       <ul className={click ? "nav-menu active" : "nav-menu"}>
         <li>
