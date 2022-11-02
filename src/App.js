b6a4883234bd7aabfceb7f0e0d1bc173f5f2748f
kimmingyu <minku4820@gmail.com>
2022-11-02 22:28:55 +0900

    Lazy loading을 통한 초기 접근 최적화

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./index.css";
 import Home from "./routes/Home";
-import About from "./routes/About";
-import Project from "./routes/Project";
-import Contact from "./routes/Contact";
 
 import { Route, Routes, Navigate } from "react-router-dom";
 import NotFound from "./routes/NotFound";
+import LoadingSpinner from "./ui/LoadingSpinner";
+
+const About = React.lazy(() => import("./routes/About"));
+const Project = React.lazy(() => import("./routes/Project"));
+const Contact = React.lazy(() => import("./routes/Contact"));
 
 function App() {
-
   async function addContactHandler(event) {
     const response = await fetch(
       "https://react-http-6ede3-default-rtdb.firebaseio.com/contacts.json",
@@ -27,14 +28,25 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/home"/>} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/contact" element ={<Contact onAddContact={addContactHandler}/>} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
+          <Route
+            path="/contact"
+            element={<Contact onAddContact={addContactHandler} />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
