a1cdc5096485a7c68d2d64a4fdde86b3a9f664be
kimmingyu <minku4820@gmail.com>
2022-11-10 02:44:50 +0900

    NotFound 페이지 수정

--- src/hooks/UpdateTitle.js
+++ src/hooks/UpdateTitle.js
@@ -4,10 +4,12 @@ export function useTitle(initialTitle) {
   const [title, setTitle] = useState(initialTitle);
 
   const updateTitle = () => {
-    
     const htmlTitle = document.querySelector("title");
+
     if (title === "Home") {
       htmlTitle.innerText = "KMG's PortFolio";
+    } else if (title === "NotFound") {
+      htmlTitle.innerText = "페이지를 찾을 수 없습니다.";
     } else {
       htmlTitle.innerText = `${title}-KMG's PortFolio`;
     }
