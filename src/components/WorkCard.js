03457b6788f35a4e46451739dca45f165e41b853
kimmingyu <minku4820@gmail.com>
2022-11-10 04:27:57 +0900

    최적화 작업

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -4,17 +4,17 @@ import "./WorkCardStyles.css";
 const WorkCard = (props) => {
   return (
     <div className="project-card">
-      <img src={props.imgsrc} alt="image" />
+      <img src={props.imgsrc} alt="project" />
       <h2 className="project-title">{props.title}</h2>
       <div className="pro-details">
         <h3>최적화 : {props.type}</h3>
         <h3>참여 : {props.part}</h3>
         <p>{props.text}</p>
         <div className="pro-btns">
-          <a href={props.view} target="_blank" className="btn">
+          <a href={props.view} target="_blank" rel="noopener noreferrer" className="btn">
             View
           </a>
-          <a href={props.source} className="btn">
+          <a href={props.source} target="_blank" rel="noopener noreferrer" className="btn">
             Source
           </a>
         </div>
