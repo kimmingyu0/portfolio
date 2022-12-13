6df2b7f2b48a138a32eb207fd8f97246b1f3bd1a
kimmingyu <minku4820@gmail.com>
2022-12-13 09:32:17 +0900

    Contact 제출 시 현재 날짜 표시 추가

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,13 +9,13 @@ const Form = (props) => {
 
   function submitHandler(event) {
     event.preventDefault();
-    
+
     if (NameRef.current.value.trim().length === 0) {
-      alert('성함을 입력해주세요.');
+      alert("성함을 입력해주세요.");
       NameRef.current.focus();
-      return
+      return;
     } else if (EmailRef.current.value.trim().length === 0) {
-      alert('연락처를 입력해주세요.');
+      alert("연락처를 입력해주세요.");
       EmailRef.current.focus();
       return;
     }
@@ -24,27 +24,40 @@ const Form = (props) => {
       name: NameRef.current.value,
       email: EmailRef.current.value,
       text: TextRef.current.value,
+      time: new Date().toLocaleString(),
     };
 
     props.onAddContact(contact);
 
-    alert('제출 되었습니다!');
-    NameRef.current.disabled=true;
-    EmailRef.current.disabled=true;
-    TextRef.current.disabled=true;
-    document.querySelector('.smbtn').classList.add('btn-light');
-    document.querySelector('.smbtn').disabled= true;
+    alert("제출 되었습니다!");
+    NameRef.current.disabled = true;
+    EmailRef.current.disabled = true;
+    TextRef.current.disabled = true;
+    document.querySelector(".smbtn").classList.add("btn-light");
+    document.querySelector(".smbtn").disabled = true;
   }
 
   return (
     <div className="form">
       <form onSubmit={submitHandler}>
         <label>성함</label>
-        <input type="text" ref={NameRef} placeholder="성함을 작성해주세요"></input>
+        <input
+          type="text"
+          ref={NameRef}
+          placeholder="성함을 작성해주세요"
+        ></input>
         <label>연락처</label>
-        <input type="text" ref={EmailRef} placeholder="이메일 또는 전화번호를 작성해주세요"></input>
+        <input
+          type="text"
+          ref={EmailRef}
+          placeholder="이메일 또는 전화번호를 작성해주세요"
+        ></input>
         <label>남기실 말씀</label>
-        <textarea row="6" placeholder="남기실 말씀을 작성해주세요" ref={TextRef} />
+        <textarea
+          row="6"
+          placeholder="남기실 말씀을 작성해주세요"
+          ref={TextRef}
+        />
         <button className="btn smbtn">저장</button>
       </form>
     </div>
